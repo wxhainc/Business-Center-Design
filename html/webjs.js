2aa9ad3431fb3f8bfea1762abe1a3e553f99d0bc
Wxha <wxhainc@gmail.com>
2025-10-23 01:11:32 +0000

    Add files via upload

---
+++ html/webjs.js
@@ -0,0 +1,83 @@
+function logohome(){
+	window.location.assign('file:///G:/website/html/homepage.html');
+}
+
+function home(){
+	window.location.assign('file:///G:/website/html/homepage.html');
+}
+
+function logout(){
+	window.location.assign('file:///G:/website/html/frontpage.html');
+}
+
+function inhomepage(){
+	window.location.assign('file:///G:/website/html/in.html');
+}
+
+function bcdhomepage(){
+	window.location.assign('file:///G:/website/html/bcd.html');
+}
+
+function wfhomepage(){
+	window.location.assign('file:///G:/website/html/wf.html');
+}
+
+function abushomepage(){
+	window.location.assign('file:///G:/website/html/aboutus.html');
+}
+
+function br(){
+	window.location.assign('file:///G:/website/html/business%20registration.html');
+}
+
+function ber(){
+	window.location.assign('file:///G:/website/html/business%20evaluation%20and%20reporting.html');
+}
+
+function bol(){
+	window.location.assign('file:///G:/website/html/business%20official%20logo.html');
+}
+
+function baf(){
+	window.location.assign('file:///G:/website/html/business%20advert%20film.html');
+}
+
+function bpd(){
+	window.location.assign('file:///G:/website/html/business%20plan%20development.html');
+}
+
+function bwd(){
+	window.location.assign('file:///G:/website/html/business%20website%20development.html');
+}
+
+function bspd(){
+	window.location.assign('file:///G:/website/html/business%20sign%20post%20development.html');
+}
+
+function bpdl(){
+	window.location.assign('file:///G:/website/html/business%20plan%20development.html');
+}
+
+function oraf(){
+	window.location.assign('file:///G:/website/html/ordersubbcdadvfilm.html');
+}
+
+function orber(){
+	window.location.assign('file:///G:/website/html/ordersubbcdfst.html');
+	
+}
+
+function orbol(){
+	window.location.assign('file:///G:/website/html/ordersubbcdbusinessdevelopment.html');
+	
+}
+
+function orpd(){
+	window.location.assign('file:///G:/website/html/ordersubbcdbusinessdevelopment.html');
+	
+}
+
+function orbwd(){
+	window.location.assign('file:///G:/website/html/ordersubbcdwebsite.html');
+	
+}
